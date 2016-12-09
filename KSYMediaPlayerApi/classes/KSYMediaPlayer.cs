f47011ee379b3ed3e6eab13a70a6d03adde45d6b
jojinshallar <jojinshallar@gmail.com>
2016-12-10 02:37:46 +0800

    V1.0

---
+++ KSYMediaPlayerApi/classes/KSYMediaPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ksyun.Media.Player
+{
+    public partial class KSYMediaPlayer
+    {
+        public override Com.Ksyun.Media.Player.Misc.PM_DInterface[] GetTrackInfo()
+        {
+            return this.GetMediaPlayerTrackInfo();
+        }
+    }
+}
